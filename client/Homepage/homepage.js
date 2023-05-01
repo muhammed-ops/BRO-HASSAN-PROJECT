96274f84f0e562e88e33413579fc10295f2d170f
Muhammed <adekolamuhammed89@gmail.com>
2023-05-01 14:59:09 +0100

    did the menu bar with javascript

---
+++ client/Homepage/homepage.js
@@ -0,0 +1,15 @@
+var icon = document.querySelector('.container ion-icon')
+var list = document.querySelector('.container nav ul')
+
+var toggle = true
+const Menu = () => {
+    if(toggle){
+    list.style.display = 'block';
+    toggle = false;
+    }else{
+    list.style.display = 'none';
+    icon.style.display = 'block';
+    toggle = true;
+    }
+}
+icon.addEventListener('click', Menu)
